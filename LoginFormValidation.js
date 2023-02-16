062c1ee9611a79bd2403a311b070189db76e36d1
Sandesh Arjun Mishra <88798964+MishraSandesh@users.noreply.github.com>
2023-02-17 01:04:33 +0530

    index.html , LoginPageStyle and LoginFormValidation added

---
+++ LoginFormValidation.js
@@ -0,0 +1,31 @@
+function validateform() {
+  var name = document.getElementById("name").value;
+  var password = document.getElementById("password").value;
+
+  if (name == null || name == "") 
+  {
+    alert("Name can't be blank");
+    return false;
+  } 
+  else if (password.length < 6) 
+  {
+    alert("Password must be at least 6 characters long.");
+    return false;
+  }
+
+  if (name == "sandesh@wishweavers.com" && password == "Sandesh@1") 
+  {
+    window.location.href = "http://www.w3schools.com";
+    return false;
+  } 
+  else if (name == "lokesh@wishweavers.com" && password == "lokesh@1") 
+  {
+    window.location.href = "http://www.w3schools.com";
+    return false;
+  } 
+  else
+  {
+    alert("Please enter valid credentials");
+    return false;
+  }
+}
