c409616e293b75788827654ae7184d88937fb536
Sandesh Arjun Mishra <88798964+MishraSandesh@users.noreply.github.com>
2023-05-02 20:14:09 +0000

    password changed to nikki

--- LoginFormValidation.js
+++ LoginFormValidation.js
@@ -23,7 +23,7 @@ function validateform() {
     window.location.href = "http://www.w3schools.com";
     return false;
   } 
-  else if (name == "nikki@wishweavers.com" && password == "nikki@1") 
+  else if (name == "nikki@wishweavers.com" && password == "nikki") 
   {
     window.location.href = "https://mishrasandesh.github.io/LovePage.github.io/";
     return false;
